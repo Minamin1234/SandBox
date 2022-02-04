43391a3662cfed71b99c57254ddc11b97b5a9da0
Minamin <minamin37345@gmail.com>
2022-02-04 16:14:59 +0900

    ABSの問題を解答．

--- ABC049C - 白昼夢/main.cpp
+++ ABC049C - 白昼夢/main.cpp
@@ -4,19 +4,60 @@ using namespace std;
 
 priority_queue<pair<int, string>> keywords;
 
+bool EraseWord(string before, string& result)
+{
+	auto copy = keywords;
+	while (!copy.empty())
+	{
+		string rword = "";
+		string r = "";
+		for (int s = 0; s < copy.top().first; s++)
+		{
+			if (copy.top().first > before.size()) goto WEND;
+			rword += before.at(s);
+			
+			
+			if (s == copy.top().first - 1)
+			{
+				for (int i = s + 1; i < before.size(); i++)
+					r += before.at(i);
+			}
+		}
+
+		if (rword == copy.top().second)
+		{
+			result = r;
+			return true;
+		}
+	WEND:
+		copy.pop();
+	
+	}
+
+	return false;
+}
+
 int main()
 {
 	string S;
-	vector<string> words;
-	
-	words.push_back("dream");
-	words.push_back("dreaner");
-	words.push_back("erase");
-	words.push_back("eraser");
+
+	keywords.push(make_pair(5, "dream"));
+	keywords.push(make_pair(7, "dreamer"));
+	keywords.push(make_pair(5, "erase"));
+	keywords.push(make_pair(6, "eraser"));
+
 
 	cin >> S;
 
-	//for(auto w : )
+	while (S.size() != 0)
+	{
+		if (EraseWord(S, S) == false)
+		{
+			cout << "NO" << endl;
+			return 0;
+		}
+	}
 
+	cout << "YES" << endl;
 	return 0;
 }
